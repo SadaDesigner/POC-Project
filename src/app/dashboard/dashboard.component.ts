784194d57bc7b6d714632862ea7b3fb1d927f6df
SadaDesigner <52165737+SadaDesigner@users.noreply.github.com>
2020-12-03 20:17:11 +0530

    multi select items feature added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,29 +5,50 @@ import { MydataService } from '../mydata.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private dataservice: MydataService) {}
 
-  constructor(private dataservice:MydataService) { }
+  todolist: any = [];
 
-  todolist:any = [];
+  showBulkActions: boolean = false;
+  selectedList: any = [];
+  selectedTitle:string;
+  titleList:any = [];
 
-  showBulkActions:boolean = false;
-  selectedList:any = [];
+  checBoxClick(e, id) {
+ 
+      // check items and push to selected list
+      if (e.target.checked) {
+
+        //console.log(id + 'checked');
+        this.selectedList.push(id);
+        this.selectedTitle = this.todolist[id - 1].title;
+        this.titleList.push(this.selectedTitle);
+
+      } else {
+        //console.log(id + 'unchecked');
+        this.selectedList = this.selectedList.filter((m) => m != id);
+        //this.titleList = this.titleList.filter((m) => !this.selectedTitle);
+      }
+
+     // console.log(this.todolist[id - 1].title);
+      ///////////////////////////////////////
 
 
-  checBoxClick(e, To) {
-    this.selectedList.push(To)
-    
-   if(this.selectedList.length > 1 ) {
-    this.showBulkActions = true;
-   }
+      //show bulk items on multiple checks condition
+      if (this.selectedList.length > 1) {
+        this.showBulkActions = true;
+      } else {
+        this.showBulkActions = false;
+      }
   }
+
   ngOnInit() {
-    this.dataservice.getdata().subscribe(data => this.todolist = data);
-
-  
+    //this.dataservice.getdata().subscribe(data => this.todolist = data);
+    this.dataservice.getdata().subscribe((data) => {
+      this.todolist = data;
+    });
   }
-
 }
